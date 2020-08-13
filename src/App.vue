e6b359317ad5fabddfa6e29b7fbcbfc52a2f74dc
Eric Neuman <eric@theneuman.com>
2020-08-13 09:17:56 -0700

    ace in

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
-  <div id="app">
+  <div id="app" class="h-100">
     <debug-tools @directory-list-item-added="addDirectoryListItem"></debug-tools>
-    <b-list-group>
-      <b-list-group-item v-for="item in DirectoryListItems" :key="item.id">
-        <directory-list-item :label="item.label" :done="item.done" :id="item.id"></directory-list-item>
-      </b-list-group-item>
-    </b-list-group>
+    <b-row class="h-100">
+      <b-col cols="3" class="h-100 overflow-auto">
+        <b-list-group>
+          <b-list-group-item v-for="item in DirectoryListItems" :key="item.id">
+            <directory-list-item
+              :label="item.label"
+              :done="item.done"
+              :id="item.id"
+              @directory-list-item-clicked="clickDirectoryListItem"
+            ></directory-list-item>
+          </b-list-group-item>
+        </b-list-group>
+      </b-col>
+      <b-col cols="9">
+        <div class="h-100">
+          <editor class="h-100" v-model="msg" @init="editorInit" lang="html" theme="chrome"></editor>
+
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -13,26 +28,57 @@
 import uniqueId from "lodash.uniqueid";
 import DirectoryListItem from "./components/DirectoryListItem";
 import DebugTools from "./components/DebugTools";
+import fs from "fs";
+require(["emmet/emmet"], function (data) {
+  // this is huge. so require it async is better
+  window.emmet = data.emmet;
+});
 
 export default {
   name: "App",
   components: {
     DebugTools,
     DirectoryListItem,
+    editor: require('vue2-ace-editor'),
   },
   data() {
     return {
       DirectoryListItems: [
+        { id: uniqueId("todo-"), label: "Learn Vue", done: false },
       ],
+      msg: "Pandas",
     };
   },
   methods: {
-    addDirectoryListItem(toDoLabel) {
+    addDirectoryListItem(DirectoryListItemLabel) {
       this.DirectoryListItems.push({
         id: uniqueId("todo-"),
-        label: toDoLabel,
+        label: DirectoryListItemLabel,
         done: false,
       });
+    },
+    clickDirectoryListItem(DirectoryListItemLabel) {
+      console.log("clickDirectoryListItem");
+      this.msg = DirectoryListItemLabel;
+
+      console.log(DirectoryListItemLabel);
+      var output = "noodles";
+      output = fs.readFileSync(DirectoryListItemLabel, "utf8", (err, data) => {
+        if (err) throw err;
+        //console.log(data);
+        output = data;
+      });
+
+      console.log(output);
+      this.msg = output;
+    },
+    editorInit() {
+      require("brace/ext/language_tools"); //language extension prerequsite...
+      require("brace/mode/html");
+      require("brace/mode/javascript"); //language
+      require("brace/mode/less");
+      require("brace/theme/chrome");
+      require("brace/snippets/javascript"); //snippet
     },
   },
 };
